15dcf5fabf72aaa4cc61bace65965674d48d3261
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-10-23 14:45:48 -0700

    scafolding

---
+++ src/linked-list/linked_list.py
@@ -0,0 +1,41 @@
+"""A linked list in python."""
+
+
+class LinkedList(object):
+    """."""
+
+    def push(self):
+        """."""
+        pass
+
+    def pop(self):
+        """."""
+        pass
+
+    def size(self):
+        """."""
+        pass
+
+    def search(self, val):
+        """."""
+        pass
+
+    def remove(self, node):
+        """."""
+        pass
+
+    def display(self):
+        """."""
+        pass
+
+    def __init__(self):
+        """."""
+        pass
+
+    def __len__(self):
+        """Return size of linked list."""
+        pass
+
+    def __str__(self):
+        """Return what the display method returns."""
+        pass
