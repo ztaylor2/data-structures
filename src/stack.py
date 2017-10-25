96ca8077ae30ae97b063b4a6efa0198797de8c23
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-10-24 18:23:16 -0700

    passing all tests

--- src/stack.py
+++ src/stack.py
@@ -1,18 +1,30 @@
 """A stack in python."""
 
+from linked_list import LinkedList
+
 
 class Stack(LinkedList):
     """A stack data structure."""
 
     def __init__(self, iterable=None):
         """Init Stack."""
+        self.list = LinkedList
+        self.list.head = None
+        self.list._counter = 0
+        if isinstance(iterable, (str, tuple, list)):
+            for item in iterable:
+                self.list.push(self, item)
 
     def __len__(self):
         """Return the size of the stack."""
-
+        return self.list.__len__(self)
 
     def push(self, value):
         """Add a value to the top of stack."""
+        self.list.push(self, value)
+        self.list._counter += 1
 
     def pop(self):
         """Remove value from top of stack and return it."""
+        self.list.pop(self)
+        self.list._counter -= 1
--- src/test_stack.py
+++ src/test_stack.py
@@ -26,10 +26,28 @@ def test_len():
     assert len(s) == 1
 
 
+def test_len2():
+    """Test the len method of Stack."""
+    from stack import Stack
+    s = Stack()
+    s.push(5)
+    s.push(6)
+    s.push(7)
+    assert len(s) == 3
+
+
 def test_pop():
     """Test pop method of Stack."""
     from stack import Stack
     s = Stack()
     s.push(5)
-    s.pop(5)
+    s.pop()
     assert len(s) == 0
+
+
+def test_stack_can_take_iterable():
+    """."""
+    from stack import Stack
+    a_list = [5, 2, 9, 0, 1]
+    s = Stack(a_list)
+    assert len(s) == 5
