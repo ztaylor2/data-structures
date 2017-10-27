ccf3cbdb1ab4757080933c21718249d3e1930ad4
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-10-27 10:42:06 -0700

    refactored tests, added better doc strings, improved a couple tests.

--- src/test_doubly_linked_list.py
+++ src/test_doubly_linked_list.py
@@ -7,49 +7,52 @@ import pytest
 def doubly_list(scope="function"):
     """Return dbly linked list for testing."""
     from doubly_linked_list import DoublyLinkedList
-    dll = DoublyLinkedList()
-    return dll
+    return DoublyLinkedList()
 
 
 def test_node_has_attributes():
-    """Test node."""
+    """Test node has prev and next_node attributes."""
     from doubly_linked_list import Node
-    n = Node(1, None, None)
+    n = Node(1, 1, 1)
     assert hasattr(n, 'data')
+    assert hasattr(n, 'next_node')
+    assert hasattr(n, 'prev')
 
 
 def test_doubly_linked_list_has_head(doubly_list):
-    """Test head."""
-    assert doubly_list.list.head is None
+    """Test head and tail on init."""
+    assert doubly_list.head is None
+    assert doubly_list.tail is None
 
 
 def test_doubly_linked_list_push_adds_new_item(doubly_list):
-    """Test push."""
+    """Test push adds value to head and tail."""
     doubly_list.push('val')
     assert doubly_list.head.data == 'val'
+    assert doubly_list.tail.data == 'val'
 
 
 def test_doubly_linked_list_counter(doubly_list):
-    """Test counter."""
+    """Test len fxn returns correct length."""
     doubly_list.push(1)
     doubly_list.push(2)
     assert len(doubly_list) == 2
 
 
 def test_doubly_linked_list_push(doubly_list):
-    """Test push."""
+    """Test push adds value to list."""
     doubly_list.push(1)
     assert len(doubly_list) == 1
 
 
 def test_doubly_linked_list_append(doubly_list):
-    """Test append."""
+    """Test append adds value to list."""
     doubly_list.append(1)
     assert len(doubly_list) == 1
 
 
 def test_doubly_linked_shift(doubly_list):
-    """Test pop."""
+    """Test pop returns correct val."""
     doubly_list.push(77)
     assert doubly_list.pop() == 77
 
@@ -67,7 +70,7 @@ def test_pop_when_empty_list_exception(doubly_list):
 
 
 def test_shift_returns_correct_val(doubly_list):
-    """Test shift."""
+    """Test shift returns correct val."""
     doubly_list.push(77)
     doubly_list.push(66)
     doubly_list.push(55)
@@ -100,7 +103,7 @@ def test_init_iterable():
 
 
 def test_remove_method(doubly_list):
-    """Test remove method."""
+    """Test remove method removes correct val."""
     doubly_list.push(3)
     doubly_list.push(2)
     doubly_list.push(1)
@@ -116,12 +119,14 @@ def test_remove_when_val_not_in_list(doubly_list):
         doubly_list.push(1)
         doubly_list.remove(2)
 
+
 def test_remove_when_one_val_in_list(doubly_list):
     """Test error for pop of empty list."""
     doubly_list.push(1)
     doubly_list.remove(1)
-    assert doubly_list.head == None
-    assert doubly_list.tail == None
+    assert doubly_list.head is None
+    assert doubly_list.tail is None
+
 
 def test_pop(doubly_list):
     """Test error for pop of empty list."""
