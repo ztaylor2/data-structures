b93a6245fb5e560324f61fd5f8f59bd52d1d8b43
jensjoh01 <johnnyj2439@gmail.com>
2017-10-30 13:14:52 -0700

    added append and appendleft function

--- src/deque.py
+++ src/deque.py
@@ -1,1 +1,16 @@
 """This is a deque data structure."""
+
+from doubly_linked_list import DoublyLinkedList
+
+
+class deque(object):
+    """Deque data structure, can add and remove from both ends."""
+
+    def __init__(self):
+        self.deque = DoublyLinkedList()
+
+    def append(self, value):
+        self.deque.append(value)
+
+    def appendleft(self, value):
+        self.deque.push(value)
