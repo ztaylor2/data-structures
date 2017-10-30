89b4a2a55b0a0c8893dbfb83f3545e684f16a3a4
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-10-30 13:14:23 -0700

    append and appendleft tests.

--- src/test_deque.py
+++ src/test_deque.py
@@ -1,1 +1,23 @@
 """Testing deque data structure."""
+
+import pytest
+
+
+
+@pytest.fixture
+def deque():
+    """A fixture for the queue."""
+    from deque import Deque
+    return Queue()
+
+
+def test_append_method(deque):
+    """Test the append method."""
+    deque.append(5)
+    assert deque.tail.data == 5
+
+
+def test_append_left(deque):
+    """Test the append left method."""
+    deque.appendleft(5)
+    assert deque.head.data == 5
