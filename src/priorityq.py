5ab48064e59e12271209ee482f67c9cbb275dc05
jensjoh01 <johnnyj2439@gmail.com>
2017-11-02 08:57:30 -0700

    finished the module for priority que, needs to be tested

--- src/priorityq.py
+++ src/priorityq.py
@@ -1,39 +1,45 @@
-"""."""
+"""
+Priority Que that takes an optional priority value and returns the 
+value with the highest priority. The default priority is 0.
+"""
 
 
-class P_Heap(object):
-    """."""
+class P_Que(object):
+    """Priority Que that returns values based on priority."""
 
     def __init__(self):
-        """."""
-        self.p_heap = {}
+        """Initialize the priority que, initializing a dictionary."""
+        self.p_que = {}
 
     def insert(self, data, priority=0):
-        """.""" 
-        if isinstance(data,(tuple, list)):
+        """Insert value and an optional priority parameter.""" 
+        if isinstance(data, (tuple, list)):
             for item in data:
                 self.insert(item, priority)
         else:
             try:
-                self.p_heap[priority].append(data)
+                self.p_que[priority].append(data)
             except KeyError:
-                self.p_heap[priority] = [data]
+                self.p_que[priority] = [data]
 
     def peek(self):
-        """."""
+        """Look at the next value that would be popped."""
         try:
-            priority = sorted(self.p_heap.keys(), reverse=True)
-            return self.p_heap[priority[0][0]]
+            priority = sorted(self.p_que.keys(), reverse=True)
+            return self.p_que[priority[0]][0]
         except IndexError:
             raise 'Priority queue is empty'
-        
+
     def pop(self):
-        """."""
-        try:    
-            priority = sorted(self.p_heap.keys(), reverse=True)              
-            return self.p_heap[priority[0]].pop(0)
-        except IndexError:
-            raise 'Priority queue is empty'
+        """Remove the highest priority value and return it. If multiple
+            values have the same priority it will return the value that
+            was inserted first."""
+        priority = sorted(self.p_que.keys(), reverse=True)
+        try:
+            try:      
+                return self.p_que[priority[0]].pop(0)
+            except IndexError:
+                raise 'Priority queue is empty'
         except TypeError:
-            pass
-            # TODO delete the key and re call pop()
+            self.p_que.pop(priority[0])
+            return self.pop()
