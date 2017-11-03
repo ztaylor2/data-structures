1fff3f0e666d32bd64ca8abe095623c977fd47b9
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-11-03 14:47:05 -0700

    nodes and edges methods done, yet to be tested.

--- src/graph.py
+++ src/graph.py
@@ -1,16 +1,16 @@
 """Module for creating a graph data structure."""
 
 class Graph(object):
-    """."""
-    
+    """A graph data structure."""
+
     def __init__(self):
         """Initialize the graph as a dictionary."""
         self.graph = {}
-    
+
     def add_nodes(self, val):
         """Add a new node with input val to the graph."""
         self.graph[val] = []
-        
+
     def add_edge(self, val1, val2):
         """Add an edge from the node val1 to the node val2.
             If val1 or val2 nodes do not exist they are created."""
@@ -23,4 +23,25 @@ class Graph(object):
         except KeyError:
             self.add_nodes(val2)
         self.graph[val1].append(val2)
-        
+
+    def nodes(self):
+        """Retrun the nodes of the graph as a list."""
+        try:
+            nodes = []
+            for key in self.graph:
+                nodes.append(key)
+            return nodes
+        except KeyError:
+            return None
+
+    def edges(self):
+        """Return the edges of the graph."""
+        try:
+            edges = []
+            for key in self.graph:
+                for i in self.graph[key]:
+                        pointer_back = key
+                        pointer_front = self.graph[key][i]
+                        edges.append((pointer_back, pointer_front))
+        except KeyError:
+            return None
