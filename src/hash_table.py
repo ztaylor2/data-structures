d2eb97fbb7860374329ee638e348846cf72241e2
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-08 08:44:21 -0800

    Built out function names.

---
+++ src/hash_table.py
@@ -0,0 +1,21 @@
+"""Hash table in python."""
+
+
+class HashTable(object):
+    """A hash table in python."""
+
+    def get(self, val):
+        """Get a value from the table."""
+        pass
+
+    def set(self, val):
+        """Set a value in the table."""
+        pass
+
+    def _hash(self, val):
+        """Hash a value."""
+        pass
+
+    def _map_index(self, hash):
+        """Map a hashed value to an index."""
+        pass
