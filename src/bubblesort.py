a6b27cc7da55cb725639b299a1163c9579d4b0bf
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-11 10:57:34 -0800

    added to readme and added timeit.

--- src/bubblesort.py
+++ src/bubblesort.py
@@ -1,4 +1,7 @@
 """Bubblesort in python."""
+import random
+import timeit
+
 
 def bubblesort(unsorted_list):
     """Sort unsorted list."""
@@ -11,3 +14,23 @@ def bubblesort(unsorted_list):
                 swapped = True
 
     return unsorted_list
+
+
+def _wrapper(func, *args, **kwargs): # pragma: no cover
+    def _wrapped(): # pragema: no cover
+        return func(*args, **kwargs)
+    return _wrapped
+
+
+if __name__ == '__main__': # pragma: no cover
+    print('Sorting list of 100 rand nums between 0-1000')
+
+    unsorted_list = []
+    for i in range(100):
+        unsorted_list.append(random.randint(0, 1000))
+
+    sorted_list = bubblesort(unsorted_list)
+
+    bubblesort_list = _wrapper(bubblesort, unsorted_list)
+    print('Time to sort 1M times:')
+    print(timeit.timeit(bubblesort_list))
