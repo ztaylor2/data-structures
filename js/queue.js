1d23d2aa59ebcb8bc60987ec254d0935eb7ac734
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-27 14:38:06 -0800

    built out enqueue peek and size methods, yet to test.

---
+++ js/queue.js
@@ -0,0 +1,46 @@
+'use strict';
+
+class Node {
+    constructor(val, next=null) {
+        this.val = val;
+        this.next = next;
+    }
+};
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this._length = 0;
+    }
+
+    enqueue(val) {
+        this.head = new Node(val, this.head)
+        this._length++;
+    }
+
+    dequeue() {
+        if (this._length === 0) {
+            throw new Error('List is empty.'); 
+        }
+        this._length--;
+    }
+
+    peek() {
+        if (this._length === 0) {
+            throw new Error('List is empty.'); 
+        }
+
+        let node = self.head;
+        while (node.next) {
+            node = node.next;
+        }
+
+        return node.val
+    }
+
+    size() {
+        return this._length
+    }
+
+
+}
